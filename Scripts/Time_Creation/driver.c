7b9376633cbcd6e17f83dbf8ee5515251c28ae94
kunmukh <kunmukh@gmail.com>
2019-02-18 14:46:53 -0600

    The Tick calulation fixed

--- Scripts/Time_Creation/driver.c
+++ Scripts/Time_Creation/driver.c
@@ -7,7 +7,7 @@
 #define MAX_CHARACTER_SIZE 32
 #define MAX_TIME_ANCHOR 64
 #define C 300000000
-#define TICK 65536
+#define TICK 65536 * 975000
 
 int main(int argc, char const *argv[])
 {
@@ -25,8 +25,8 @@ int main(int argc, char const *argv[])
     }
     printf("\n");    
 
-    //Tx = 16440 + (d / c) * TICK + 16440 + (NOISE / c) * TICK
-    //TICK = 65536 tick / sec
+    //Tx = 16440 + ((d * c * TICK) + 16440 + (NOISE / c) * TICK
+    //TICK = 65536 tick / sec * 975000
     //NOISE = 0.003 m
     //d = 10 m
     //c = 300000000 m / sec
@@ -38,11 +38,11 @@ int main(int argc, char const *argv[])
         oTx[i] = obuf[i];
     }
 
-    int anchorA[MAX_TIME_ANCHOR] = {'0'};
+    uint64_t anchorA[MAX_TIME_ANCHOR] = {'0'};
     int indexA = 0, dA = atoi(argv[2]);
-    int anchorB[MAX_TIME_ANCHOR] = {'0'};
+    uint64_t anchorB[MAX_TIME_ANCHOR] = {'0'};
     int indexB = 0, dB = atoi(argv[3]);
-    int anchorC[MAX_TIME_ANCHOR] = {'0'};
+    uint64_t anchorC[MAX_TIME_ANCHOR] = {'0'};
     int indexC = 0, dC = atoi(argv[4]);
     int anchorChoice[3 * MAX_TIME_ANCHOR]  = {'0'};
     int indexChoice = 0;
@@ -53,43 +53,43 @@ int main(int argc, char const *argv[])
     {
     	if(rand() % 2 == 0)
     	{
-    		anchorA[indexA] = oTx[i] + 16440 + ((dA / C) * TICK ) + 16440 + (0.003 / C) * TICK;
+    		anchorA[indexA] = oTx[i] + 16440 + (dA * C * TICK ) + 16440 + (0.003 * C * TICK);
     		indexA++;
     		anchorChoice[indexChoice] = 0;
     		indexChoice++;
     	}else if (rand() % 3 == 0)
     	{
-    		anchorB[indexB] = oTx[i] + 16440 + ((dB / C) * TICK)  + 16440 + (0.003 / C) * TICK;
+    		anchorB[indexB] = oTx[i] + 16440 + (dB * C * TICK)  + 16440 + (0.003 * C * TICK);
     		indexB++;
     		anchorChoice[indexChoice] = 1;
     		indexChoice++;
     	}else
     	{
-    		anchorC[indexC] = oTx[i] + 16440 + ((dC / C) * TICK)  + 16440 + (0.003 / C) * TICK;
+    		anchorC[indexC] = oTx[i] + 16440 + (dC * C * TICK)  + 16440 + (0.003 * C * TICK);
     		indexC++;
     		anchorChoice[indexChoice] = 2;
     		indexChoice++;
     	}
     }
 
-    printf("Anchor A: ");
+    printf("Anchor A: \n");
     for (int i = 0; i < indexA; i++) 
     {
-        printf("%d ", anchorA[i]);
+        printf("%lu \n", anchorA[i]);
     }
     printf("\n");
 
-    printf("Anchor B: ");
+    printf("Anchor B: \n");
     for (int i = 0; i < indexB; i++) 
     {
-        printf("%d ", anchorB[i]);
+        printf("%lu \n", anchorB[i]);
     }
     printf("\n");
 
-    printf("Anchor C: ");
+    printf("Anchor C: \n");
     for (int i = 0; i < indexC; i++) 
     {
-        printf("%d ", anchorC[i]);
+        printf("%lu \n", anchorC[i]);
     }
     printf("\n");
 
@@ -100,24 +100,20 @@ int main(int argc, char const *argv[])
     	printf("Transmission %0d ", i );
     	if (anchorChoice[i] == 0)
     	{
-    		printf("Anchor A: %d \n", anchorA[anchorATransmit]);
+    		printf("Anchor A: %lu \n", anchorA[anchorATransmit]);
     		anchorATransmit++;
     	}
     	if (anchorChoice[i] == 1)
     	{
-    		printf("Anchor B: %d \n", anchorB[anchorBTransmit]);
+    		printf("Anchor B: %lu \n", anchorB[anchorBTransmit]);
     		anchorBTransmit++;
     	}
     	if (anchorChoice[i] == 2)
     	{
-    		printf("Anchor C: %d \n", anchorC[anchorCTransmit]);
+    		printf("Anchor C: %lu \n", anchorC[anchorCTransmit]);
     		anchorCTransmit++;
     	}
     }
-
-
-    
-
 
 	return 0;    
 
