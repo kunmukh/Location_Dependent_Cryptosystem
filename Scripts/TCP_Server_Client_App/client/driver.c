12bcfe5c312fb879312a7946af2604810acf9f15
kunmukh <kunmukh@gmail.com>
2019-02-28 00:48:21 -0600

    The TCP client and server send and receives anch #, Tx and Offset

--- Scripts/TCP_Server_Client_App/client/driver.c
+++ Scripts/TCP_Server_Client_App/client/driver.c
@@ -4,16 +4,16 @@
 #include <stdlib.h> 
 #include <netinet/in.h> 
 #include <string.h> 
-#include <inttypes.h>
 #include <unistd.h>
 #include <arpa/inet.h>
+#include <inttypes.h>
 
 #define PORT 8080 
 
 int64_t S64(const char *s) {
   int64_t i;
   char c ;
-  int scanned = sscanf(s, "%" SCNd64 "%c", &i, &c);
+  int scanned = sscanf(s, "%"  PRIu64 "%c", &i, &c);
   if (scanned == 1) return i;
   if (scanned > 1) {
     // TBD about extra data found
@@ -23,15 +23,12 @@ int64_t S64(const char *s) {
   return 0;  
 }
 
-
 int main(int argc, char const *argv[]) 
 { 
-    
-    int sock = 0, valread = 1; 
+    int sock = 0; 
     struct sockaddr_in serv_addr; 
-    char *startTransmit = "Start Transmit";
-    char *received = "CLient Acknowledgement"; 
-    
+    char *hello = "Hello from client"; 
+    char buffer[1024] = {0}; 
     if ((sock = socket(AF_INET, SOCK_STREAM, 0)) < 0) 
     { 
         printf("\n Socket creation error \n"); 
@@ -54,86 +51,65 @@ int main(int argc, char const *argv[])
     { 
         printf("\nConnection Failed \n"); 
         return -1; 
-    }     
+    } 
 
-    char bufferAnchor[1] = {0};     
-    char bufferTx[32] = {0};
-    //char bufferOffset[21] = {0};
-    char bufferData[32] = {0};   
+    send(sock , hello , strlen(hello) , 0 );
+    printf("Ready to receive\n");
 
-    int packet_content = 0;
+    char * received = "Packet acknowledgement received";
 
-    int anchorNumber = 0;
-    uint64_t Tx = 0;
-    //uint64_t TxOffset = 0;
-    int data[32] = {'0'};
+    send(sock , received , strlen(received) , 0 ); 
+    int counter = 0;
 
-    send(sock , startTransmit , strlen(startTransmit) , 0 );
+    uint64_t anchorNumber = 0;
+    uint64_t tx = 0;
+    uint64_t offset = 0;
 
-    while(valread == 1)
-    {        
-        if (packet_content == 0)
-        {            
-            memset (bufferAnchor, 0, 1);
+    char txBuff[21]= {0};
+    char anchorNumberbuff[21]= {0};
+    char offsetBuff[21]= {0};   
 
-            valread = read( sock , bufferAnchor, 1);  
+    int index = 0;
 
-            anchorNumber = bufferAnchor[0];
-            printf("Anchor Number: %d\n", anchorNumber);
+    FILE * receptionFile;
+    receptionFile = fopen(argv[1],"w");
 
-            packet_content++;
+    while (read( sock , buffer, 1024) > 1)
+    {
+        printf("\nPacket Content: %s\n", buffer);
+        
+        for(int i =0; i <1; i++)
+            {anchorNumberbuff[index] = buffer[i]; index++;}
+        index = 0;        
+        for(int i =1; i <21; i++)
+            {txBuff[index] = buffer[i]; index++;}
+        index = 0;        
+        for (int i = 21; i < strlen(buffer); i++)
+            {offsetBuff[index] = buffer[i]; index++;}
+        printf("\n");index = 0;        
 
-        }
-        else if (packet_content == 1)
-        {
-            memset (bufferTx, 0, 32);
+        anchorNumber =  S64(anchorNumberbuff);
+        tx = S64(txBuff);
+        offset = S64(offsetBuff);        
 
-            valread = read( sock , bufferTx, 32);
+        printf("Anchor Number: %lu\n", anchorNumber);
+        printf("Tx: %lu\n", tx);
+        printf("Offset %lu\n", offset);
 
-            printf("STx: %s\n", bufferTx);            
-            Tx = S64(bufferTx);
-            printf("Tx: %lu\n", Tx);
+        fprintf(receptionFile, "%lu ", anchorNumber);
+        fprintf(receptionFile, "%lu\n",tx);
 
-            packet_content++;
+        send(sock , received , strlen(received) , 0 ); 
 
-            packet_content++;
-        }
-        else if (packet_content == 2)
-        {
-            //memset(bufferOffset, 0, 21);
-            //valread = read( sock , bufferOffset, 21);
-            //TxOffset = S64(bufferOffset);
-            //printf("Tx Offset: %lu\n", TxOffset);
-            packet_content++;
-        }
-        else if (packet_content == 3)
-        {
-            memset (bufferData, 0, 32);
+        counter++;
 
-            valread = read( sock , bufferData, 32); 
-
-            printf("Packet Content: \n");           
-            for (int i = 0; i < 32; i++)
-            {
-                data[i] = bufferData[i];
-                printf("%d ", data[i] );
-            }
-            printf("\n"); 
-
-            packet_content++;
-        }
-
-        if (packet_content % 4 == 0)
-        {
-            packet_content = 0;
-        }
-
-        send(sock , received , strlen(received) , 0 );
-       
-    }
-          
+        memset(buffer, 0, sizeof(char) * 1024);
+        memset(txBuff, 0, sizeof(char) * 21);
+        memset(anchorNumberbuff, 0, sizeof(char) * 21);
+        memset(offsetBuff, 0, sizeof(char) * 21);
+    } 
     
+    printf("Number of Packet sent: %d\n", counter );
 
     return 0; 
-    
 } 
--- Scripts/TCP_Server_Client_App/server/driver.c
+++ Scripts/TCP_Server_Client_App/server/driver.c
@@ -1,3 +1,4 @@
+// Server side C/C++ program to demonstrate Socket programming 
 #include <unistd.h> 
 #include <stdio.h> 
 #include <sys/socket.h> 
@@ -5,29 +6,25 @@
 #include <netinet/in.h> 
 #include <string.h> 
 #include <inttypes.h>
-
 #define PORT 8080 
-
-#define MAX_CHARACTER_SIZE 32
-
 int main(int argc, char const *argv[]) 
 { 
-    int server_fd, new_socket; 
+    int server_fd, new_socket, valread; 
     struct sockaddr_in address; 
     int opt = 1; 
-
-    int addrlen = sizeof(address);      
-       
+    int addrlen = sizeof(address); 
+    char buffer[1024] = {0}; 
+    
     // Creating socket file descriptor 
     if ((server_fd = socket(AF_INET, SOCK_STREAM, 0)) == 0) 
     { 
         perror("socket failed"); 
         exit(EXIT_FAILURE); 
     } 
-       
+    
     // Forcefully attaching socket to the port 8080 
     if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR | SO_REUSEPORT, 
-                                                  &opt, sizeof(opt))) 
+                                                &opt, sizeof(opt))) 
     { 
         perror("setsockopt"); 
         exit(EXIT_FAILURE); 
@@ -35,104 +32,72 @@ int main(int argc, char const *argv[])
     address.sin_family = AF_INET; 
     address.sin_addr.s_addr = INADDR_ANY; 
     address.sin_port = htons( PORT ); 
-       
+    
     // Forcefully attaching socket to the port 8080 
-    if (bind(server_fd, (struct sockaddr *)&address,  
-                                 sizeof(address))<0) 
+    if (bind(server_fd, (struct sockaddr *)&address, 
+                                sizeof(address))<0) 
     { 
         perror("bind failed"); 
         exit(EXIT_FAILURE); 
     } 
-
     if (listen(server_fd, 3) < 0) 
     { 
         perror("listen"); 
         exit(EXIT_FAILURE); 
     } 
-    if ((new_socket = accept(server_fd, (struct sockaddr *)&address,  
-                       (socklen_t*)&addrlen))<0) 
+    if ((new_socket = accept(server_fd, (struct sockaddr *)&address, 
+                    (socklen_t*)&addrlen))<0) 
     { 
         perror("accept"); 
         exit(EXIT_FAILURE); 
     } 
-    
-    char bufferAnchor[1] = {0};     
-    char bufferTx[21] = {0};
-    //char bufferOffset[21] = {0};
-    char bufferData[32] = {0};
-    char buffer[1024] = {0};
 
+    valread = read( new_socket , buffer, 1024);     
+    printf("%s\n",buffer ); 
+    char * message = "I know you";
+
+    int counter = 0;
+
+    char packet [100] = {0};
+    memset(packet, 0, sizeof(char) * 100);
     FILE * transmissionFile;
     transmissionFile = fopen(argv[1],"r");
 
-    FILE * dataFile;
-    dataFile = fopen(argv[2],"r");
-    
-    int AESbuf = 0;    
-    int anchorNumber = 0;
+    uint64_t anchorNumber = 0;
     uint64_t Tx = 0;
-    int packet_content = 0;
+    uint64_t offset = 599;
 
-    while(read( new_socket , buffer, 1024))
+    char txBuff[21]= {0};
+    char anchorNumberbuff[21]= {0};
+    char offsetBuff[21]= {0};
+
+    while (read( new_socket , buffer, 1024) > 1)
     {
-        printf("%s\n",buffer ); 
+        printf("%s\n",buffer );
 
-        if (packet_content == 0)
-        {
-            memset (bufferAnchor, 0, 1);
-            if (fscanf(transmissionFile, "%d" , &anchorNumber) != EOF)
-            {
-                bufferAnchor[0] = anchorNumber;
-            }
-            printf("Anchor: %c\n", bufferAnchor[0] );
-            send(new_socket , bufferAnchor , strlen(bufferAnchor) , 0 );
-            packet_content++;
-            printf("Anchor Number Sent\n");
-        }
-        else if (packet_content == 1)
-        {
-            memset (bufferTx, 0, 21);
-            if (fscanf(transmissionFile, "%lu" , &Tx) != EOF)
-            {                
-                sprintf(bufferTx, "%" PRIu64, Tx);
-            }
-            printf("Tx: %s\n", bufferTx);            
-            send(new_socket , bufferTx , strlen(bufferTx) , 0 );
-            packet_content++;
+        if (fscanf(transmissionFile, "%lu" , &anchorNumber) == EOF){break;}
+        sprintf(anchorNumberbuff, "%" PRIu64, anchorNumber);
+        if (fscanf(transmissionFile, "%lu" , &Tx) == EOF){break;}
+        sprintf(txBuff, "%" PRIu64, Tx);
+        sprintf(offsetBuff, "%" PRIu64, offset);        
 
-            packet_content++;
-            printf("Tx sent\n");
-        }
-        else if (packet_content == 2)
-        {
-            //memset(bufferOffset, 0, 21);
-            //send(new_socket , bufferOffset , strlen(bufferOffset) , 0 );
-            //packet_content++;
-        }
-        else if (packet_content == 3)
-        {
-            memset (bufferData, 0, 32);
-            for (int i = 0; i < MAX_CHARACTER_SIZE; i++)
-            {
-                if (fscanf(dataFile, "%d" , &AESbuf) != EOF)
-                {
-                     bufferData[i] = AESbuf;
-                }                                        
-            }
-            printf("Data %s\n", bufferData);
-            send(new_socket , bufferData , strlen(bufferData) , 0 );
-            packet_content++;
-            printf("Data sent.\n");
-        }                
-        memset (buffer, 0, 1024);
-        
-        if (packet_content % 4 == 0)
-        {
-            packet_content = 0;
-        }
-    }  
+        memcpy(&packet[0], anchorNumberbuff, sizeof(anchorNumberbuff));
+        memcpy(&packet[strlen(anchorNumberbuff)], txBuff, sizeof(txBuff));
+        memcpy(&packet[strlen(anchorNumberbuff) + strlen(txBuff)], offsetBuff, 
+            sizeof(offsetBuff));
 
-    fclose(dataFile);
-    fclose(transmissionFile);
+        printf("%s\n", packet);
+
+        send(new_socket , packet , strlen(packet) , 0 ); 
+        printf("Message sent\n\n");       
+        memset(packet, 0, sizeof(char) * 100);
+
+        counter++;
+        if (counter > 100) {break;}
+    }
+
+    printf("%d\n", counter );
+    
+
     return 0; 
 } 
